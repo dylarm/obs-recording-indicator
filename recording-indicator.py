c0e81e8f2c3ad6ac3117b256b0cd8858bd6888e4
Dylan Armitage <dylanjarmitage@gmail.com>
2021-04-16 21:55:02 -0700

    Initial commit

---
+++ recording-indicator.py
@@ -0,0 +1,76 @@
+import obspython as obs
+
+VERSION = "v0.0.1d"
+
+
+class _G:
+    def __init__(self, source_name=None):
+        self.source_name = source_name
+
+    def set_visible(self):
+        current_scene = obs.obs_scene_from_source(obs.obs_frontend_get_current_scene())
+        scene_item = obs.obs_scene_find_source(current_scene, self.source_name)
+        obs.obs_sceneitem_set_visible(scene_item, True)
+        obs.obs_scene_release(current_scene)
+
+    def set_invisible(self):
+        current_scene = obs.obs_scene_from_source(obs.obs_frontend_get_current_scene())
+        scene_item = obs.obs_scene_find_source(current_scene, self.source_name)
+        obs.obs_sceneitem_set_visible(scene_item, False)
+        obs.obs_scene_release(current_scene)
+
+
+g = _G()
+
+
+def make_visible():
+    g.set_visible()
+
+
+def make_invisible():
+    g.set_invisible()
+
+
+def script_update(settings):
+    g.source_name = obs.obs_data_get_string(settings, "source")
+
+
+def script_properties():
+    props = obs.obs_properties_create()
+    p = obs.obs_properties_add_list(
+        props,
+        "source",
+        "Source",
+        obs.OBS_COMBO_TYPE_EDITABLE,
+        obs.OBS_COMBO_FORMAT_STRING,
+    )
+    sources = obs.obs_enum_sources()
+    if sources is not None:
+        for source in sources:
+            source_id = obs.obs_source_get_unversioned_id(source)
+            name = obs.obs_source_get_name(source)
+            obs.obs_property_list_add_string(p, name, source_id)
+        obs.source_list_release(sources)
+    return props
+
+
+def script_description():
+    return f"Show a source only when recording \n{VERSION}"
+
+
+def on_event(event):
+    if event == obs.OBS_FRONTEND_EVENT_RECORDING_STARTED:
+        # Make source visible
+        make_visible()
+    elif event == obs.OBS_FRONTEND_EVENT_RECORDING_STOPPED:
+        # Make source invisible again
+        make_invisible()
+    elif event == obs.OBS_FRONTEND_EVENT_SCENE_CHANGED:
+        if obs.obs_frontend_recording_active():
+            make_visible()
+        else:
+            make_invisible()
+
+
+def script_load(settings):
+    obs.obs_frontend_add_event_callback(on_event)
